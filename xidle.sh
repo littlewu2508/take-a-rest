5d975451cb406b7df0fe817218d9d39ae73eb3fb
Yiyang Wu <xgreenlandforwyy@gmail.com>
2022-12-06 16:39:13 +0800

    Introducing idle program similar to swayidle in X

---
+++ xidle.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+TIMEOUT=$(($4 * 1000))  # s -> ms
+TIMEOUT_CMD=$5
+RESUME_CMD=$7
+
+while true; do
+	idle_time_ms=$(xprintidle)
+	if [ ${idle_time_ms} -gt ${TIMEOUT} ]; then
+		${TIMEOUT_CMD}
+		prevous_idle_time_ms=$(xprintidle)
+		break
+	fi
+	sleep 1;
+done
+
+while true; do
+	idle_time_ms=$(xprintidle)
+	if [ ${idle_time_ms} -lt ${prevous_idle_time_ms} ]; then
+		${RESUME_CMD}
+		break
+	fi
+	sleep 1;
+done
